fda7c13e9f1ca6a98e043a9bfab3618477ac4f1c
sthDINESH <sthapit.dnsh@gmail.com>
2025-09-16 14:34:30 +0100

    handle post for collaboration request

--- about/views.py
+++ about/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+from django.contrib import messages
 from .models import About
 from .forms import CollaborateForm
 
@@ -9,6 +10,18 @@ from .forms import CollaborateForm
 def about_me(request):
     # return HttpResponse("This will include About me")
     about = About.objects.order_by("-updated_on").first()
+
+    if request.method == "POST":
+        collaborate_form = CollaborateForm(data=request.POST)
+        if collaborate_form.is_valid():
+            collaboration = collaborate_form.save(commit=False)
+            collaboration.read = False
+            collaboration.save()
+
+            messages.add_message(
+                request, messages.SUCCESS,
+                'Collaboration request received! I endeavour to respond within 2 working days.'
+            )
 
     collaborate_form = CollaborateForm()
 
@@ -20,5 +33,3 @@ def about_me(request):
             "collaborate_form": collaborate_form,
         },
     )
-
-
