fc857694d1b7859f1e57b5dc990b568e87906e59
sthDINESH <sthapit.dnsh@gmail.com>
2025-09-17 09:48:21 +0100

    docstrings for PEP8

--- about/views.py
+++ about/views.py
@@ -8,6 +8,17 @@ from .forms import CollaborateForm
 
 
 def about_me(request):
+    """
+    Renders the most recent information on the website author
+    and allows user collaboration
+    Displays an individual instance of :model:`about.About`
+    **Context**
+    ``about``
+        The most recent instance of :model:`about.About`
+    ``collaborate_form``
+        An instance of :form:`about.CollaborateForm`
+    :template:`about/about.html`
+    """
     # return HttpResponse("This will include About me")
     about = About.objects.order_by("-updated_on").first()
 
--- blog/models.py
+++ blog/models.py
@@ -5,7 +5,12 @@ from cloudinary.models import CloudinaryField
 STATUS = ((0, "Draft"), (1, "Published"))
 
 # Create your models here.
+
+
 class Post(models.Model):
+    """
+    Stores a single blog post entry related to :model:`auth.User`.
+    """
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="blog_posts")
@@ -24,17 +29,17 @@ class Post(models.Model):
 
 
 class Comment(models.Model):
+    """
+    Stores a single comment related to :model:`Post` and :model:`auth.User`.
+    """
     post = models.ForeignKey(Post,on_delete=models.CASCADE,related_name="comments")
     author = models.ForeignKey(User,on_delete=models.CASCADE,related_name="commenter")
     body = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     approved = models.BooleanField(default=False)
-    
+
     class Meta:
         ordering = ["created_on"]
-    
+
     def __str__(self):
         return f"Comment {self.body} by {self.author}"
-
-
-
